6f11c88f6c81b3c20dd7678c9894489dc4f16020
hstampfl <heinz.st@mpf.li>
2015-03-16 00:14:02 +0100

    Update makepackage.sh
    Rename

--- utils/makepackage.sh
+++ utils/makepackage.sh
@@ -4,19 +4,19 @@ version=$1
 [ ! -e "dkms.conf" ] && echo "start this script from base directory" && exit 1
 rm -rf build || (echo "unable to clean build directory" && exit 1)
 mkdir build || (echo "unable to create build directory" && exit 1)
-mkdir build/mk-arcade-joystick-rpi-$version || (echo "unable to create package directory" && exit 1)
-rootdir="build/mk-arcade-joystick-rpi-${version}"
+mkdir build/gpio-arcade-joystick-rpi-$version || (echo "unable to create package directory" && exit 1)
+rootdir="build/gpio-arcade-joystick-rpi-${version}"
 
 echo "copying resources"
-srcdir="$rootdir/usr/src/mk_arcade_joystick_rpi-${version}"
-sharedir="$rootdir/usr/share/mk_arcade_joystick_rpi-${version}"
+srcdir="$rootdir/usr/src/gpio_arcade_joystick_rpi-${version}"
+sharedir="$rootdir/usr/share/gpio_arcade_joystick_rpi-${version}"
 
 cp -r DEBIAN "$rootdir"
 
 mkdir -p "$srcdir"
 cp dkms.conf "$srcdir"
 cp Makefile "$srcdir"
-cp mk_arcade_joystick_rpi.c "$srcdir"
+cp gpio_arcade_joystick_rpi.c "$srcdir"
 
 mkdir -p "$sharedir"
 cp LICENSE "$sharedir"
@@ -27,4 +27,4 @@ sed -i "s/\$MKVERSION/${version}/g" $srcdir/* $rootdir/DEBIAN/control $rootdir/D
 echo "building dpkg"
 
 cd build
-sudo dpkg-deb --build "mk-arcade-joystick-rpi-${version}/"
+sudo dpkg-deb --build "gpio-arcade-joystick-rpi-${version}/"
